62f043abd0cdc0b3b6a2ad8677f271e928f99858
crqcastro <cesarrqc@gmail.com>
2021-01-15 15:05:21 -0300

    D - Primeiro commit

---
+++ src/main/java/br/com/cesarcastro/biblioteko/BibliotekoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesarcastro.biblioteko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotekoApplication.class, args);
+	}
+
+}
